a22605a261aba1d95fd004b9bf00c31f9644b809
Srijan Tripathi <47470062+srijan2498@users.noreply.github.com>
2024-11-19 00:04:39 +0530

    Update ProtectedRoute.js

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -14,7 +14,7 @@ export default function ProtectedRoute({ children }) {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "/api/v1/user/getUserData",
+        "https://doctor-appointment-booking-mern-app.vercel.app/api/v1/user/getUserData",
         { token: localStorage.getItem("token") },
         {
           headers: {
