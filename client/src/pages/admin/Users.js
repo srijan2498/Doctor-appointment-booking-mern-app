195606c97e076a66d8706af584e6761c1be43aa5
Srijan Tripathi <47470062+srijan2498@users.noreply.github.com>
2024-11-19 00:08:36 +0530

    Update Users.js

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
   //getUsers
   const getUsers = async () => {
     try {
-      const res = await axios.get("/api/v1/admin/getAllUsers", {
+      const res = await axios.get("https://doctor-appointment-booking-mern-app.vercel.app/api/v1/admin/getAllUsers", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
