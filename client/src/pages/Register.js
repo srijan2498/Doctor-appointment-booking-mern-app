df289436539ffd3f9a27387e2f0c5356e018058c
Srijan Tripathi <47470062+srijan2498@users.noreply.github.com>
2024-11-19 00:08:05 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
   const onfinishHandler = async (values) => {
     try {
       dispatch(showLoading());
-      const res = await axios.post("/api/v1/user/register", values);
+      const res = await axios.post("https://doctor-appointment-booking-mern-app.vercel.app/api/v1/user/register", values);
       dispatch(hideLoading());
       if (res.data.success) {
         message.success("Register Successfully!");
